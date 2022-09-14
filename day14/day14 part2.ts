30aad1ab8c5458ef0aebe961d875c990d9613d37
JulianNF <nf.julian+github@gmail.com>
2022-09-14 17:54:46 +0200

    Day 14 * added gitignore

--- day14/day14 part2.ts
+++ day14/day14 part2.ts
@@ -8,12 +8,12 @@ import { readFileSync } from 'fs';
 
 let polymerTemplate: string = '';
 let insertionRules: insertionRules = {};
-convertRawInputTextToTemplateAndInsertionRules('day14/day14 input.txt');
+convertInputTextToTemplateAndInsertionRules('day14/day14 input.txt');
 
 console.log('insertionRUles:', insertionRules);
 
 generateAdditionalRules();
-console.log('insertionRUlessss:', insertionRules);
+console.log('insertionRulessss:', insertionRules);
 
 // let resultingPolymer = repeatedlyInsertPolymersIntoTemplate(polymerTemplate, 10);
 // console.log(findQuantityDifferenceBetweenLeastAndMostCommonElementsInPolymer(resultingPolymer));
@@ -30,75 +30,75 @@ console.log('\ntotal ms: ', performance.now() - startMS);
 // ------------------------------------------------------------ //
 
 interface insertionRule {
-	oneInsertion: string;
-	fiveInsertions?: string;
+    oneInsertion: string;
+    fiveInsertions?: string;
 }
 
 interface insertionRules {
-	[key: string]: insertionRule
+    [key: string]: insertionRule;
 }
 
-function convertRawInputTextToTemplateAndInsertionRules(fileName: string): void {
-	const rawText = readFileSync(fileName, 'utf-8');
-	const textRows = rawText.replace(/\r\n/g, '\n').split('\n'); // Regex to account for possbility of IDE being set to CRLF instead of LF end of lines
+function convertInputTextToTemplateAndInsertionRules(fileName: string): void {
+    const rawText = readFileSync(fileName, 'utf-8');
+    const textRows = rawText.replace(/\r\n/g, '\n').split('\n'); // Regex to account for possbility of IDE being set to CRLF instead of LF end of lines
 
-	polymerTemplate = textRows[0];
+    polymerTemplate = textRows[0];
 
-	for (let i = 2; i < textRows.length; i++) {
-		let initialPair = textRows[i][0] + textRows[i][1];
-		// let result = textRows[i][0] + textRows[i][6] + textRows[i][1];
+    for (let i = 2; i < textRows.length; i++) {
+        let initialPair = textRows[i][0] + textRows[i][1];
+        // let result = textRows[i][0] + textRows[i][6] + textRows[i][1];
 
-		let newRule: insertionRule = {
-			oneInsertion: textRows[i][0] + textRows[i][6] + textRows[i][1],
-			fiveInsertions: undefined,
-		};
-		insertionRules[initialPair] = newRule;
-	}
+        let newRule: insertionRule = {
+            oneInsertion: textRows[i][0] + textRows[i][6] + textRows[i][1],
+            fiveInsertions: undefined,
+        };
+        insertionRules[initialPair] = newRule;
+    }
 }
 
 function generate5xInsertionRules(): void {
-	for (let rule of Object.keys(insertionRules)) {
-		let result = repeatedlyInsertPolymersIntoTemplate(rule, 2);
-		console.log('new result:', result);
-		insertionRules[rule] = result;
-	}
+    for (let rule of Object.keys(insertionRules)) {
+        let result = repeatedlyInsertPolymersIntoTemplate(rule, 2);
+        console.log('new result:', result);
+        insertionRules[rule] = result;
+    }
 }
 
 function insertElementsIntoPolymer(startPolymer: string): string {
-	let newPolymer: string = '';
+    let newPolymer: string = '';
 
-	for (let i = 0; i < startPolymer.length - 1; i++) {
-		let polymerA = startPolymer[i];
-		let polymerB = startPolymer[i + 1];
-		let currentPair = polymerA + polymerB;
-		newPolymer += polymerA + insertionRules[currentPair];
-	}
-	newPolymer += startPolymer[startPolymer.length - 1];
+    for (let i = 0; i < startPolymer.length - 1; i++) {
+        let polymerA = startPolymer[i];
+        let polymerB = startPolymer[i + 1];
+        let currentPair = polymerA + polymerB;
+        newPolymer += polymerA + insertionRules[currentPair];
+    }
+    newPolymer += startPolymer[startPolymer.length - 1];
 
-	return newPolymer;
+    return newPolymer;
 }
 
 function repeatedlyInsertPolymersIntoTemplate(startPolymer: string, numberOfRepetitions: number): string {
-	let polymer = startPolymer;
-	for (let i = 1; i <= numberOfRepetitions; i++) {
-		polymer = insertElementsIntoPolymer(polymer);
-	}
-	return polymer;
+    let polymer = startPolymer;
+    for (let i = 1; i <= numberOfRepetitions; i++) {
+        polymer = insertElementsIntoPolymer(polymer);
+    }
+    return polymer;
 }
 
 function findQuantityDifferenceBetweenLeastAndMostCommonElementsInPolymer(polymer: string): number {
-	let elementCounts: { [key: string]: number } = {};
+    let elementCounts: { [key: string]: number } = {};
 
-	for (let element of polymer) {
-		elementCounts[element] = (elementCounts[element] || 0) + 1;
-	}
-	console.log('counts:', elementCounts);
+    for (let element of polymer) {
+        elementCounts[element] = (elementCounts[element] || 0) + 1;
+    }
+    console.log('counts:', elementCounts);
 
-	let counts = Object.values(elementCounts);
-	let maxCount = Math.max(...counts);
-	let minCount = Math.min(...counts);
+    let counts = Object.values(elementCounts);
+    let maxCount = Math.max(...counts);
+    let minCount = Math.min(...counts);
 
-	return maxCount - minCount;
+    return maxCount - minCount;
 }
 
 console.log('==============================\n');
